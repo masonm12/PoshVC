9ac472e4db69451ddfdff002b0f187d2f858c013
Mathew Mason <masonm12@gmail.com>
2014-05-03 22:55:55 -0600

    Functions to print VC install paths.

---
+++ psvc.ps1
@@ -0,0 +1,49 @@
+$yearVersionMap = @{
+    '2013' = '12.0';
+    '2012' = '11.0';
+    '2010' = '10.0';
+    '2008' = '9.0';
+}
+
+# returns registry root (wow6432node, etc)
+function getSoftwareRegRoot($hkey) {
+    $regRoot = 'HKLM:\SOFTWARE\Wow6432Node'
+    # if an pointer is 4 bytes, then we're 32-bit!
+    if ([IntPtr]::size -eq 4) {
+        $regRoot = 'HKLM:\SOFTWARE'
+    }
+
+    $regRoot
+}
+
+# tests if a property exists on an object
+function doesPropertyExist($object, $property) {
+    $object.PSObject.Properties.Match($property).Count -ne 0
+}
+
+# takes a version (e.g. 12.0, 2013, etc)
+# returns VC install path (e.g. C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\)
+function getVCInstallPath($version) {
+    $vsRoot = Join-Path $(getSoftwareRegRoot) 'Microsoft\VisualStudio\SxS\VC7'
+    # be sure registry entry exists at all
+    if (Test-Path $vsRoot) {
+        $vsProp = Get-ItemProperty $vsRoot
+        if (doesPropertyExist $vsProp $version) {
+            $vsProp.$version
+        }
+        elseif ($yearVersionMap[$version]) {
+            $version = $yearVersionMap[$version]
+            if (doesPropertyExist $vsProp $version) {
+                $vsProp.$version
+            }
+        }
+    }
+}
+
+# prints a sorted array of visual c version numbers
+function printInstalledVCVersions() {
+    $versions = $yearVersionMap.GetEnumerator() | sort @{e={$_.Value -as [Decimal]}; descending=$true} |
+        ? {getVCInstallPath $_.Value}
+    "Detected Visual Studio Installs:`n"
+    $versions | % {"`tVisual Studio " + $_.Key + ' (' + $_.Value + ')'}
+}
