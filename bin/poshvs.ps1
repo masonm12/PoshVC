af8488274d0e0e8cfce0bf8a61d5f8f7aa1c2386
Mathew Mason <masonm12@gmail.com>
2015-05-29 01:21:53 -0700

    Changing references of Visual C and VC to Visual Studio and VS.

--- bin/poshvs.ps1
+++ bin/poshvs.ps1
@@ -5,8 +5,8 @@ $psvsVersion = '1.1.0'
 $commandHelp = @{
     'version' = 'Print the version number.';
     'help' = 'Print this help message.';
-    'list' = 'Print all detected versions of Visual C.';
-    'use' = 'Import vcvarsall for a given version of Visual C (e.g. 2013, 10.0, etc).';
+    'list' = 'Print all detected versions of Visual Studio.';
+    'use' = 'Import vcvarsall for a given version of Visual Studio (e.g. 2013, 10.0, etc).';
 }
 $psvsUsage = "usage $psvsName [--version] [--help] command args
     
@@ -31,7 +31,7 @@ optional arguments:
     --help $($commandHelp['help'])
 
 arguments:
-    version      Version of Visual C vcvarsall to try and import.
+    version      Version of Visual Studio vcvarsall to try and import.
                  (default: latest)
     architecture Tools architecture to load, passed to vcvarsall.
                  (default: $env:PROCESSOR_ARCHITECTURE)
@@ -61,8 +61,8 @@ function doesPropertyExist($object, $property) {
 }
 
 # takes a version (e.g. 12.0, 2013, etc)
-# returns VC install path (e.g. C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\)
-function getVCInstallPath($version) {
+# returns VC directory in Visual Studio install path (e.g. C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\)
+function getVSInstallPath($version) {
     $vsRoot = Join-Path $(getSoftwareRegRoot) 'Microsoft\VisualStudio\SxS\VC7'
     # be sure registry entry exists at all
     if (Test-Path $vsRoot) {
@@ -79,18 +79,18 @@ function getVCInstallPath($version) {
     }
 }
 
-# prints a sorted array of visual c version numbers
-function printInstalledVCVersions() {
+# prints a sorted array of visual studio version numbers
+function printInstalledVSVersions() {
     $versions = $yearVersionMap.GetEnumerator() | sort @{e={$_.Value -as [Decimal]}; descending=$true} |
-        ? {getVCInstallPath $_.Value}
+        ? {getVSInstallPath $_.Value}
     "Detected Visual Studio Installs:`n"
     $versions | % {"`    Visual Studio " + $_.Key + ' (' + $_.Value + ')'}
 }
 
-# returns the highest version of vc installed
-function getNewestVCInstallPath() {
+# returns the highest version of vs installed
+function getNewestVSInstallPath() {
     $yearVersionMap.GetEnumerator() | sort @{e={$_.Value -as [Decimal]}; descending=$true} |
-        ? {getVCInstallPath $_.Value} | % {$_.Value} | select -first 1
+        ? {getVSInstallPath $_.Value} | % {$_.Value} | select -first 1
 }
 
 # executes a batch file, and captures the environment variable settings
@@ -121,24 +121,24 @@ function invalidArguments($arguments = $args) {
     errorOut "Invalid arguments $arguments"
 }
 
-# will attempt to use the given VC version
-function useVCVersion($version, $architecture) {
+# will attempt to use the given VS version
+function useVSVersion($version, $architecture) {
     if ($version -eq 'latest') {
-        $version = getNewestVCInstallPath
+        $version = getNewestVSInstallPath
         if (!$version) {
-            errorOut 'No version of Visual C was found.'
+            errorOut 'No version of Visual Studio was found.'
         }
     }
 
-    $vcPath = getVCInstallPath $version
-    if (!$vcPath) {
-        errorOut "Visual C version $version not found."
+    $vsPath = getVSInstallPath $version
+    if (!$vsPath) {
+        errorOut "Visual Studio version $version not found."
     }
 
-    $batPath = Join-Path $vcPath 'vcvarsall.bat'
+    $batPath = Join-Path $vsPath 'vcvarsall.bat'
     execBatchFile $batPath $architecture
 
-    "Using Visual C $version $architecture"
+    "Using Visual Studio $version $architecture"
 }
 
 # checks if a value exists in an array list
@@ -179,7 +179,7 @@ if ($commands[0] -eq 'list') {
         invalidArguments $commands
     }
     else {
-        printInstalledVCVersions
+        printInstalledVSVersions
     }
     exit 0
 }
@@ -191,17 +191,17 @@ if ($commands[0] -eq 'use') {
         exit 0
     }
     
-    $vcVersion = 'latest'
+    $vsVersion = 'latest'
     $architecture = $env:PROCESSOR_ARCHITECTURE.ToLower()
     if ($commands) {
-        $vcVersion = $commands[0].ToString().ToLower()
+        $vsVersion = $commands[0].ToString().ToLower()
         $commands.removeAt(0)
     }
     if ($commands) {
         $architecture = $commands[0].ToString().ToLower()
         $commands.removeAt(0)
     }
-    useVCVersion $vcVersion $architecture
+    useVSVersion $vsVersion $architecture
     exit 0
 }
 
